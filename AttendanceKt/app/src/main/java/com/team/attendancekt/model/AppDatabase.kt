4f7eb59df86fbec67c90c755046b58d4cb342440
Theinmwe Naing <theinmwenaing22@gmail.com>
2019-12-28 10:53:36 +0630

    implement Attendance.

--- AttendanceKt/app/src/main/java/com/team/attendancekt/model/AppDatabase.kt
+++ AttendanceKt/app/src/main/java/com/team/attendancekt/model/AppDatabase.kt
@@ -2,14 +2,40 @@ package com.team.attendancekt.model
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverter
+import androidx.room.TypeConverters
+import com.team.attendancekt.model.dao.AttendanceDao
 import com.team.attendancekt.model.dao.MemberDao
 import com.team.attendancekt.model.entity.Member
+import org.joda.time.DateTime
+import org.joda.time.DateTimeZone
 
-@Database(entities = [
-    Member::class
-], version = 1, exportSchema = false)
+@Database(
+    entities = [
+        Member::class
+    ], version = 1, exportSchema = false
+)
+@TypeConverters(com.team.attendancekt.model.TypeConverters::class)
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun memberDao(): MemberDao
+    abstract fun attendanceDao(): AttendanceDao
+}
 
+class TypeConverters {
+
+    companion object {
+
+        @JvmStatic
+        @TypeConverter
+        fun getLocalDateTime(timeStamp: Long): DateTime {
+            return DateTime(timeStamp, DateTimeZone.getDefault())
+        }
+
+        @JvmStatic
+        @TypeConverter
+        fun setLocalDateTime(dateTime: DateTime): Long {
+            return dateTime.millis
+        }
+    }
 }
--- AttendanceKt/app/src/main/java/com/team/attendancekt/model/entity/Attendance.kt
+++ AttendanceKt/app/src/main/java/com/team/attendancekt/model/entity/Attendance.kt
@@ -1,10 +1,26 @@
 package com.team.attendancekt.model.entity
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
+import org.joda.time.DateTime
+import org.joda.time.LocalDateTime
 
-@Entity
+@Entity(
+    foreignKeys = [
+        ForeignKey(
+            parentColumns = ["id"],
+            childColumns = ["member_id"],
+            entity = Member::class
+        )
+    ]
+)
 data class Attendance(
     @PrimaryKey(autoGenerate = true)
-    val id: Long
+    val id: Long = 0,
+    @ColumnInfo(name = "event_time")
+    val eventTime: DateTime = DateTime.now(),
+    @ColumnInfo(name = "member_id")
+    val memberId: Int = 0
 )
