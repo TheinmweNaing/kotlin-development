f04bce4cd30d516a28ba7b8e7c15b6722156efe9
Theinmwe Naing <theinmwenaing@op-j.jp>
2020-01-02 11:38:00 +0630

    implement member attendance.

--- AttendanceKt/app/src/main/java/com/team/attendancekt/ui/attendance/MemberAttendanceFragment.kt
+++ AttendanceKt/app/src/main/java/com/team/attendancekt/ui/attendance/MemberAttendanceFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DividerItemDecoration
 import com.team.attendancekt.R
 import kotlinx.android.synthetic.main.fragment_member_attendance.*
@@ -41,6 +42,10 @@ class MemberAttendanceFragment : Fragment() {
             setHasFixedSize(true)
             addItemDecoration(DividerItemDecoration(view.context, DividerItemDecoration.VERTICAL))
             adapter = this@MemberAttendanceFragment.adapter
+        }
+
+        fab.setOnClickListener {
+            findNavController().navigate(R.id.action_memberAttendanceFragment_to_memberAttendanceEditFragment)
         }
     }
 }
--- AttendanceKt/app/src/main/java/com/team/attendancekt/ui/attendance/edit/MemberAttendanceEditFragment.kt
+++ AttendanceKt/app/src/main/java/com/team/attendancekt/ui/attendance/edit/MemberAttendanceEditFragment.kt
@@ -1,4 +1,37 @@
 package com.team.attendancekt.ui.attendance.edit
 
-class MemberAttendanceEditFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import com.team.attendancekt.R
+import kotlinx.android.synthetic.main.fragment_attendance_edit.*
+
+class MemberAttendanceEditFragment : Fragment() {
+
+    lateinit var memberAttendanceEditViewModel: MemberAttendanceEditViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        memberAttendanceEditViewModel = ViewModelProviders.of(this)[MemberAttendanceEditViewModel::class.java]
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_attendance_edit, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btnSave.setOnClickListener {
+            //TODO
+        }
+
+    }
 }
