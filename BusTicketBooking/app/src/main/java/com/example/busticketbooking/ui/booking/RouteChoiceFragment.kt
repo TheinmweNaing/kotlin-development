b55777157ddec26f0e7199808263413a920019e5
Theinmwe Naing <theinmwenaing22@gmail.com>
2020-02-02 11:27:14 +0630

    update layout.

--- BusTicketBooking/app/src/main/java/com/example/busticketbooking/ui/booking/RouteChoiceFragment.kt
+++ BusTicketBooking/app/src/main/java/com/example/busticketbooking/ui/booking/RouteChoiceFragment.kt
@@ -3,6 +3,7 @@ package com.example.busticketbooking.ui.booking
 import android.app.DatePickerDialog
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
@@ -10,6 +11,7 @@ import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import com.example.busticketbooking.R
 import kotlinx.android.synthetic.main.fragment_route_choice.*
+import org.joda.time.LocalDate
 
 class RouteChoiceFragment : Fragment() {
 
@@ -24,32 +26,37 @@ class RouteChoiceFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val adapter = ArrayAdapter<String>(view.context, android.R.layout.simple_dropdown_item_1line)
         val station = resources.getStringArray(R.array.bus_station)
-        adapter.addAll(station.toList())
+        val adapter =
+            ArrayAdapter<String>(view.context, android.R.layout.simple_list_item_1, station)
 
         auto_edit_from.setAdapter(adapter)
-        auto_edit_from.setDropDownBackgroundResource(R.color.colorAccent)
         auto_edit_to.setAdapter(adapter)
-        auto_edit_to.setDropDownBackgroundResource(R.color.colorAccent)
 
-        ed_date.setOnClickListener {
-            DatePickerDialog(view.context,
-                DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
-                    val date = "$year/$month/$dayOfMonth"
-                    ed_date.setText(date)
-                }, 0, 0, 0
-            ).show()
+        val today = LocalDate.now()
+        ed_date.setOnTouchListener { v, event ->
+            if (event.action == MotionEvent.ACTION_DOWN) {
+                DatePickerDialog(
+                    view.context,
+                    DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
+                        val date = "$year/${month + 1}/$dayOfMonth"
+                        ed_date.setText(date)
+                    }, today.year, today.monthOfYear - 1, today.dayOfMonth).show()
+                v.requestFocus()
+            }
+            return@setOnTouchListener true
         }
 
         val seats = Array(10) { (it + 1).toString() }
-        ed_seats.setOnClickListener {
-            AlertDialog.Builder(view.context)
-                .setItems(seats) { di, i ->
-                    ed_seats.setText((i + 1).toString())
-                    di.dismiss()
-                }
-                .show()
+        ed_seats.setOnTouchListener { v, event ->
+            if (event.action == MotionEvent.ACTION_DOWN) {
+                AlertDialog.Builder(view.context)
+                    .setItems(seats) { di, i ->
+                        ed_seats.setText((i + 1).toString())
+                        di.dismiss() }.show()
+                v.requestFocus()
+            }
+            return@setOnTouchListener true
         }
     }
 }
