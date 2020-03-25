8fe58e41f19e31983ca09b41dead03f4c10dd67a
Theinmwe Naing <theinmwenaing@op-j.jp>
2020-03-25 15:21:50 +0630

    update layout and fragment.

--- lyrics/app/src/main/java/com/example/lyrics/MainActivity.kt
+++ lyrics/app/src/main/java/com/example/lyrics/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.lyrics
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.layout_appbar.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+    }
+
+    fun showProgress() {
+        progressBar.visibility = View.VISIBLE
+    }
+
+    fun hideProgress() {
+        progressBar.visibility = View.GONE
     }
 }
--- lyrics/app/src/main/java/com/example/lyrics/ui/LyricsFragment.kt
+++ lyrics/app/src/main/java/com/example/lyrics/ui/LyricsFragment.kt
@@ -1,10 +1,12 @@
 package com.example.lyrics.ui
 
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
 import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -12,22 +14,22 @@ import androidx.navigation.fragment.findNavController
 import com.example.lyrics.MainActivity
 import com.example.lyrics.R
 import com.google.android.material.snackbar.Snackbar
-import kotlinx.android.synthetic.main.fragment_lyrics.*
+import kotlinx.android.synthetic.main.layout_lyrics.*
 
 class LyricsFragment : Fragment() {
 
     lateinit var viewModel: LyricsViewModel
 
     companion object {
-        val ARTIST = "artist"
-        val TITLE = "title"
+        const val ARTIST = "artist"
+        const val TITLE = "title"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setHasOptionsMenu(true)
 
-        val activity: MainActivity = requireActivity() as MainActivity
+        val activity = requireActivity() as MainActivity
         activity.supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
@@ -42,7 +44,8 @@ class LyricsFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-        progressBar.visibility = View.VISIBLE
+        val activity = requireActivity() as MainActivity
+        activity.showProgress()
         viewModel = ViewModelProvider(this)[LyricsViewModel::class.java]
 
         val artist = arguments?.getString(ARTIST)
@@ -50,18 +53,19 @@ class LyricsFragment : Fragment() {
 
         viewModel.loadLyrics(artist, title)
         viewModel.lyrics.observe(viewLifecycleOwner, Observer {
-            progressBar.visibility = View.GONE
+            activity.hideProgress()
             txtLyrics.text = it
             txtArtist.text = artist
             txtTitle.text = title
         })
 
         viewModel.result.observe(viewLifecycleOwner, Observer {
-            progressBar.visibility = View.GONE
+            activity.hideProgress()
             if (!it) view?.apply {
                 Snackbar.make(this, "Not found.", Snackbar.LENGTH_SHORT).show()
             }
         })
+        txtLyrics.movementMethod = ScrollingMovementMethod()
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -73,7 +77,6 @@ class LyricsFragment : Fragment() {
 
     override fun onDestroyView() {
         super.onDestroyView()
-        val activity: MainActivity = requireActivity() as MainActivity
-        activity.supportActionBar?.setDisplayHomeAsUpEnabled(false)
+        (activity as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(false)
     }
 }
